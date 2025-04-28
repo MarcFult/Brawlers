f52d60a97968cccb95433e14a8251e2d1169df67
MarcFult <131759204+MarcFult@users.noreply.github.com>
2025-04-28 16:52:25 +0200

    implemented springboot

---
+++ backendSpring/brawling/src/test/java/com/brawls/brawling/BrawlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brawls.brawling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrawlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
